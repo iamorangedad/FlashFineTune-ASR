0c5d1889d26f8174c27bac256a521d22efc594c3
zhumj <allenexecutivezhu@gmail.com>
2025-12-08 22:29:34 -0800

    feat: update config for service

--- session_log/src/config.py
+++ session_log/src/config.py
@@ -2,9 +2,17 @@ import os
 
 
 class Config:
+    # ASR service
+    API_HOST = "0.0.0.0"
+    API_PORT = 8000
+    API_URL = f"http://127.0.0.1:{API_PORT}/transcribe"
+
+    # websocket
+    WS_URL = f"ws://127.0.0.1:{API_PORT}/ws/realtime"
+
     # NATS
     NATS_URL = os.getenv("NATS_URL", "nats://nats:4222")
-    LOG_SUBJECT = "asr.logs.new"
+    LOG_SUBJECT = "asr.inference.stream"
 
     # MinIO (S3)
     S3_ENDPOINT = os.getenv("S3_ENDPOINT", "http://10.0.0.56:39001")
