e73d10b8f9309930cebfcbad87b5f3b7be7dc1e7
zhumj <allenexecutivezhu@gmail.com>
2025-12-11 11:06:48 -0800

    feat: update dev config for NATS, MongoDB

--- session_log/src/config.py
+++ session_log/src/config.py
@@ -11,15 +11,15 @@ class Config:
     WS_URL = f"ws://127.0.0.1:{API_PORT}/ws/realtime"
 
     # NATS
-    NATS_URL = os.getenv("NATS_URL", "nats://nats:4222")
+    NATS_URL = os.getenv("NATS_URL", "http://10.0.0.27:4222")
     LOG_SUBJECT = "asr.inference.stream"
 
+    # MongoDB
+    MONGO_URI = os.getenv("MONGO_URI", "http://10.0.0.27:27017")
+    MONGO_DB = "asr_data"
+
     # MinIO (S3)
-    S3_ENDPOINT = os.getenv("S3_ENDPOINT", "http://10.0.0.56:39001")
+    S3_ENDPOINT = os.getenv("S3_ENDPOINT", "http://10.0.0.27:39001")
     S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "admin")
     S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "password123")
     S3_BUCKET = "audio"
-
-    # MongoDB
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
-    MONGO_DB = "asr_data"
